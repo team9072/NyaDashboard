65d3ac9f7bb9062463f4e8daae945f7dd7d8ebce
TigerBots <118773412+TigerBotsCSP@users.noreply.github.com>
2023-12-01 18:03:33 -0500

    try to use react

---
+++ app.jsx
@@ -0,0 +1,11 @@
+const root = document.getElementById('root')
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, Electron!</h1>
+    </div>
+  )
+}
+console.log("help")
+ReactDOM.render(<App />, root)
